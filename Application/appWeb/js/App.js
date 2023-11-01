35d396bed8d26889124e1cd9b8893a4364d176ab
Veetorio <ettorevitorio98@gmail.com>
2023-11-01 04:24:00 -0300

    criado o projeto spring e fim do front

--- Application/appWeb/js/App.js
+++ Application/appWeb/js/App.js
@@ -1,1 +1,30 @@
+let Global_nome = document.getElementById("nickname")
+let Global_email = document.getElementById("email")
+let Global_senha = document.getElementById("nickname")
 
+const entidade = {
+        nome: Global_nome.value,
+        email: Global_email.value,
+        senha: Global_senha.value
+    }
+
+function conection(){
+    const url = "http//localhost:8080/post";
+    fetch(url,{
+        headers :{
+            'Accept':'Application/json',
+            'content-type': 'application/json'
+        },
+        method : 'POST',
+        body : JSON.stringify(entidade)
+       
+    }).then((resp) => {console.log(resp)})
+}
+
+
+
+
+
+function coletar() {
+    conection(); 
+}
