53a73d563e4c8334f9b3fc9c2354bdd37caf8e6f
vee/torio <134203950+veetorio@users.noreply.github.com>
2023-11-06 13:03:47 -0300

    gfgffgfgfgfd

--- Application/appBack/project/src/main/java/com/spring/project/models/Cadastrado.java
+++ Application/appBack/project/src/main/java/com/spring/project/models/Cadastrado.java
@@ -17,36 +17,4 @@ public class Cadastrado {
     private String email;
     @Column(name = "senha",length = 10)
     private String senha;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 }
