c877394d020f6d310965ba53d443531bc5b3cab6
vee/torio <134203950+veetorio@users.noreply.github.com>
2023-11-06 13:02:11 -0300

    Tratamento de erros

--- Application/appWeb/js/App.js
+++ Application/appWeb/js/App.js
@@ -1,36 +1,39 @@
-const form = document.querySelector('form')
-const Gnome = document.querySelector('#nickname')
-const Gemail = document.querySelector('#email')
-const Gsenha = document.querySelector('#senha')
+const form = document.querySelector("form");
+const Gnome = document.querySelector("#nickname");
+const Gemail = document.querySelector("#email");
+const Gsenha = document.querySelector("#senha");
 
-
-
-
-function conect(){
-    fetch('http://localhost:8080/database/enviar', {
-        headers: {
-            'Accept': "application/json",
-            'Content-Type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify({
-            email: Gemail.value,
-            nome: Gnome.value,
-            senha: Gsenha.value
-        })
+function conect() {
+  fetch("http://localhost:8080/database/enviar", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      email: Gemail.value,
+      nome: Gnome.value,
+      senha: Gsenha.value,
+    }),
+  }).then((Promise) => {
+    if(Promise.status == 201){
+        window.alert("Cadastro feito com sucesso");
     }
-    ).then(Promise => window.alert("Cadastro feito com sucesso")).catch(window.alert(" seus dados não foram enviado por conta de algum erro "))
+    else{
+        if(Promise.status == 500){
+
+            window.alert("Você inseriu algum dado repetido")
+        }
+    }
+  });
 }
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    conect();
-}
-);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  conect();
+});
 
-
-function limpar(){
-    Gnome.value = '';
-    Gemail.value = '';
-    Gsenha.value = '';
-
+function limpar() {
+  Gnome.value = "";
+  Gemail.value = "";
+  Gsenha.value = "";
 }
