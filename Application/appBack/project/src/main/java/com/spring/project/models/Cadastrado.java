3356f7e714ac896855ac0e0e72992364a779d6b9
vee/torio <134203950+veetorio@users.noreply.github.com>
2023-11-06 11:03:03 -0300

    hholhohohohoho

--- Application/appBack/project/src/main/java/com/spring/project/models/Cadastrado.java
+++ Application/appBack/project/src/main/java/com/spring/project/models/Cadastrado.java
@@ -1,13 +1,10 @@
 package com.spring.project.models;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.util.QTypeContributor;
-@Data
+
 @Entity
-@Table(name = "Tabela-Cadastrados")
+@Table(name = "tb_Cadastros")
 public class Cadastrado {
 
     @Id
@@ -20,5 +17,35 @@ public class Cadastrado {
     @Column(name = "senha",length = 10)
     private String senha;
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
