858e773629ca2eedd2c2136e9e528933a7cb0aef
Veetorio <ettorevitorio98@gmail.com>
2023-11-10 06:42:16 -0300

    Update commit services
    -service
    -docker

--- Application/appBack/project/src/main/java/com/spring/project/Controllers/PostCadastro.java
+++ Application/appBack/project/src/main/java/com/spring/project/Controllers/PostCadastro.java
@@ -1,14 +1,9 @@
 package com.spring.project.Controllers;
-import com.spring.project.Interfaces.crudCadastros;
+import com.spring.project.Service.ServiceCad;
 import com.spring.project.models.Cadastrado;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 
@@ -16,25 +11,16 @@ import java.util.List;
 @CrossOrigin("*")
 @RequestMapping("/database")
 public class PostCadastro {
-    @Autowired
-    private crudCadastros dao;
-    private PasswordEncoder codificador = new BCryptPasswordEncoder();
+    ServiceCad services;
 
+    @PostMapping("/enviar")
+        public ResponseEntity<Cadastrado> criarUsuario(@RequestBody Cadastrado User){
+        return ResponseEntity.status(HttpStatus.CREATED).body(services.serviceNovoUsuario(User));
 
-
-@GetMapping("/Cadastrados")
-    public   ResponseEntity<List<Cadastrado>>  listaCadastrado(){
-        List<Cadastrado> ListaDeCadastrados = (List<Cadastrado>) dao.findAll();
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(ListaDeCadastrados);
-}
-@PostMapping("/enviar")
-    public ResponseEntity<Cadastrado> userNew(@RequestBody Cadastrado novoCadastro){
-    String senhaCodificada =  codificador.encode(novoCadastro.getSenha());
-    novoCadastro.setSenha(senhaCodificada);
-    Cadastrado newuser = dao.save(novoCadastro);
-    return ResponseEntity.status(HttpStatus.CREATED).body(newuser);
-}
-
-
+    }
+    @GetMapping("/listar")
+    public ResponseEntity<List<Cadastrado>> listarCadastrados(){
+        return ResponseEntity.status(HttpStatus.OK).body(services.serviceList());
+    }
 
 }
---
+++ Application/appBack/project/src/main/java/com/spring/project/Service/ServiceCad.java
@@ -0,0 +1,31 @@
+package com.spring.project.Service;
+
+import com.spring.project.Interfaces.crudCadastros;
+import com.spring.project.models.Cadastrado;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
+
+public class ServiceCad {
+
+    @Autowired
+    private crudCadastros dao;
+    private PasswordEncoder codificador = new BCryptPasswordEncoder();
+
+    public List<Cadastrado> serviceList(){
+        List<Cadastrado> ListaDeCadastrados = (List<Cadastrado>) dao.findAll();
+        return ListaDeCadastrados;
+    }
+
+    public Cadastrado serviceNovoUsuario(Cadastrado novoCadastro){
+        String senhaCodificada =  codificador.encode(novoCadastro.getSenha());
+        novoCadastro.setSenha(senhaCodificada);
+        Cadastrado newuser = dao.save(novoCadastro);
+        return newuser;
+    }
+
+}
